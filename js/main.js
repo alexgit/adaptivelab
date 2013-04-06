17a2496644ffb83ddb1a5e6dbff8edba63b05233
alexgit <alex.tashev@gmail.com>
2013-04-06 11:10:55 +0100

    Add initial viewmodel and tweet template and hook it up to Adaptive Lab's json endpoint as a starting point.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,53 @@
 require(['jquery', 'vendor/knockout'], function($, ko) {
 
+  var resource = {
+    url: 'http://adaptive-test-api.herokuapp.com/tweets.json',
+    fetchNewTweets: function() {
+      return $.ajax({
+        url: this.url,
+        dataType: 'json'
+      });
+    }
+  };
+
+  function Tweet(handle, time, sentiment, message) {
+    this.handle = handle;
+    this.time = time;
+    this.sentiment = sentiment;
+    this.message = message;
+    this.formattedTime = time; //todo: do formatting
+  }
+
+  function createTweet(tweetObj) {
+    return new Tweet(tweetObj.user_handle, tweetObj.created_at, tweetObj.sentiment, tweetObj.message);
+  }
+
+  var viewModel = {
+    tweets: ko.observableArray()
+  };
+
+  viewModel.hasNoTweets = ko.computed(function() {
+    return this.tweets().length === 0;
+  }, viewModel);
+
+
+  $(function() {
+
+    ko.applyBindings(viewModel, document.getElementById('container'));
+
+    resource.fetchNewTweets()
+      .done(function(response) {
+        var tweets = ko.utils.arrayMap(response, createTweet);
+
+        ko.utils.arrayForEach(tweets, function(t) {
+          viewModel.tweets.push(t);
+        });
+      })
+      .fail(function(error) {
+        alert('oops: ' + error);
+      });
+  });
+
 
 
 });

440ae05dcd92398d9edf45c7e36272ee24d9e475
alexgit <alex.tashev@gmail.com>
2013-04-06 13:31:47 +0100

    Add sorting by sentiment and date created.

--- js/main.js
+++ js/main.js
@@ -23,18 +23,27 @@ require(['jquery', 'vendor/knockout', 'tweetfilter', 'notyconfig'], function($, 
     }
   };
 
-  function Tweet(handle, time, sentiment, message) {
+  function Tweet(id, handle, time, sentiment, message) {
     var date = new Date(time);
 
+    this.dateCreated = date;
+    this.id = id;
     this.handle = handle;
     this.time = date.getHours() + ':' + date.getMinutes();
-    this.sentiment = sentimentMap.visualise(sentiment);
+    this.visualSentiment = sentimentMap.visualise(sentiment);
     this.message = message;
     this.formattedTime = this.time; //todo: do formatting
+    this.sentiment = sentiment;
   }
 
   function createTweet(tweetObj) {
-    return new Tweet(tweetObj.user_handle, tweetObj.created_at, tweetObj.sentiment, tweetObj.message);
+    return new Tweet(
+      tweetObj.id,
+      tweetObj.user_handle,
+      tweetObj.created_at,
+      tweetObj.sentiment,
+      tweetObj.message
+    );
   }
 
   var filter = new TweetFilter();
@@ -42,18 +51,31 @@ require(['jquery', 'vendor/knockout', 'tweetfilter', 'notyconfig'], function($, 
   var viewModel = {
     loading: ko.observable(false),
     tweets: ko.observableArray(),
+    sortBy: ko.observable('id'),
+    sortOrder: ko.observable('desc'),
+
+    flipSortOrder: function() {
+      this.sortOrder(this.sortOrder() === 'asc' ? 'desc' : 'asc');
+    },
+    sortTweets: function(by) {
+      if(by === this.sortBy()) {
+        this.flipSortOrder();
+      } else {
+        this.sortOrder('desc');
+        this.sortBy(by);
+      }
+    },
     fetchNew: function() {
       this.loading(true);
       adaptiveLab.fetchNewTweets()
       .done(function(response) {
         var filtered = filter.filterTweets(response);
-
         if(!filtered.length) {
           noty({text: "No new tweets"});
+          return;
         }
 
         var tweets = ko.utils.arrayMap(filtered, createTweet);
-
         ko.utils.arrayForEach(tweets, function(t) {
           viewModel.tweets.unshift(t);
         });
@@ -77,10 +99,39 @@ require(['jquery', 'vendor/knockout', 'tweetfilter', 'notyconfig'], function($, 
     return this.loading() ? 'Loading...' : 'MOAR';
   }, viewModel);
 
+  viewModel.sortedTweets = ko.computed(function() {
+    var order = this.sortOrder();
+    var sortBy = this.sortBy();
 
+    var sortFunc = function(a, b) {
+      if(a.localeCompare) {
+        return a.localeCompare(b);
+      } else {
+        return a - b;
+      }
+    };
+
+    if(order === 'asc') {
+      return this.tweets().sort(function(t1, t2) {
+        return sortFunc(t1[sortBy], t2[sortBy]);
+      });
+    } else {
+      return this.tweets().sort(function(t1, t2) {
+        return sortFunc(t2[sortBy], t1[sortBy]);
+      });
+    }
+
+  }, viewModel);
+
+  var container = $('#container');
 
   $(function() {
-    ko.applyBindings(viewModel, document.getElementById('container'));
+    ko.applyBindings(viewModel, container[0]);
     viewModel.fetchNew();
+
+    container.on('click', 'a.sorting-link', function() {
+      var sortBy = $(this).attr('data-sort-by');
+      viewModel.sortTweets(sortBy);
+    });
   });
 });
